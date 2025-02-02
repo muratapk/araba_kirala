2cb08b88da39cb90f34afb48de4afc3a8d5d5a75
muratapk <muratapk@hotmail.com>
2025-02-02 18:50:41 +0300

    Add project files.

---
+++ araba_kirala/Models/Brands.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace araba_kirala.Models
+{
+    public class Brands
+    {
+        [Key]
+        public int BrandID { get; set; }
+        public string BrandName { get; set; } = string.Empty;
+        public string Model { get; set; } = string.Empty;
+        public int? ModelYear { get; set; } 
+        public int? DailyPrice { get; set; }
+        virtual public List<Cars>? Cars { get; set; }
+        //bir aracın birden fazla Markası olabilir
+
+    }
+}
---
+++ araba_kirala/Models/Cars.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace araba_kirala.Models
+{
+    public class Cars
+    {
+        [Key]
+        public int CarID { get; set; }
+        public int? BrandID { get; set; }
+        public int? ColorID { get; set; }
+        public bool Status { get; set; } = false;
+        virtual public List<Rentals>? Rentals { get; set; }
+        //bir araç birden fazla kiralanabilir
+        virtual public Colors?  Colors { get; set; }
+        virtual public Brands? Brands { get; set; }  
+        
+    }
+}
