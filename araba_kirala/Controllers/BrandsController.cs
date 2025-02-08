89b51a5883ec3327915beca0f60ee282029628ec
muratapk <muratapk@hotmail.com>
2025-02-08 15:59:17 +0300

    Brands Oluşturulması

---
+++ araba_kirala/Controllers/BrandsController.cs
@@ -0,0 +1,24 @@
+﻿using araba_kirala.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace araba_kirala.Controllers
+{
+    public class BrandsController : Controller
+    {
+        private readonly Context _context;
+        //DATABASE BAĞLANTISI OLUŞTUR
+        public BrandsController(Context context)
+        {
+            //SİSTEM AÇILIR AÇILMAZ DATABASE BAĞLAN
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var listem = _context.Brands.ToList();
+            //tüm markaları listesini al lambda linq
+            //select * from Brands 
+            return View(listem);
+        }
+    }
+}
