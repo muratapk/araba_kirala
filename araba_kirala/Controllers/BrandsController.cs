b9df9b8efe8fd0fbca523f32e8257b616bcdb2d0
muratapk <muratapk@hotmail.com>
2025-02-09 15:10:52 +0300

    son işlem

--- araba_kirala/Controllers/BrandsController.cs
+++ araba_kirala/Controllers/BrandsController.cs
@@ -1,4 +1,5 @@
 ﻿using araba_kirala.Data;
+using araba_kirala.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace araba_kirala.Controllers
@@ -19,6 +20,31 @@ namespace araba_kirala.Controllers
             //tüm markaları listesini al lambda linq
             //select * from Brands 
             return View(listem);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Brands gelen)
+        {
+            _context.Brands.Add(gelen);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult Delete(int? id) 
+        {
+            var bul = _context.Brands.Where(x => x.BrandID == id).FirstOrDefault();
+            return View(bul);
+        }
+        [HttpPost]
+        public IActionResult Delete(Brands gelen)
+        {
+            var bul = _context.Brands.Find(gelen.BrandID);
+            _context.Brands.Remove(bul);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
